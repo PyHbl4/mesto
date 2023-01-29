7ee08f45a406c8cdd8990a512c690b47d5ba51fe
PyHbl4 <maxim_runkov@mail.ru>
2023-01-29 12:56:40 +0500

    feat: завершена разработка десктопной версии

---
+++ index.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let closeButton = document.querySelector('.popup__close-icon');
+    let editButton = document.querySelector('.profile__edit-button');
+    let profileName = document.querySelector('.profile__title');
+    let profileProfession = document.querySelector('.profile__subtitle');
+    let popup = document.querySelector('.popup');
+    let inputName = document.querySelector('.edit-form__input_name');
+    let inputProfession = document.querySelector('.edit-form__input_profession');
+    let formBody = document.querySelector('.edit-form')
+    function openPopup() {
+        popup.classList.add('popup_opened');
+        document.body.style.overflow = 'hidden';
+        inputName.value = `${profileName.textContent}`;
+        inputProfession.value = `${profileProfession.textContent}`;
+    }
+    function closePopup() {
+        popup.classList.remove('popup_opened');
+        document.body.style.overflow = 'auto';
+        inputName.value = '';
+        inputProfession.value = '';
+    }
+    editButton.addEventListener('click', openPopup);
+    closeButton.addEventListener('click', closePopup);
+    window.addEventListener('keyup', function(key) {
+        if (key.code === 'Escape') {
+            closePopup();
+        }
+    })
+    function handleFormSubmit (evt) {
+        evt.preventDefault();
+        profileName.textContent = `${inputName.value}`;
+        profileProfession.textContent = `${inputProfession.value}`;
+        closePopup();
+    }
+    formBody.addEventListener('submit', handleFormSubmit); 
+});
